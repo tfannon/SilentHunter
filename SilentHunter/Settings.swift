29d7039f6566a638e97ff95d0da8bba24e9be268
Tommy Fannon <tfannon@gmail.com>
2014-10-11 22:49:16 -0400

    settings

---
+++ SilentHunter/Settings.swift
@@ -0,0 +1,17 @@
+//
+//  Settings.swift
+//  SilentHunter
+//
+//  Created by Tommy Fannon on 10/11/14.
+//  Copyright (c) 2014 Crazy8Dev. All rights reserved.
+//
+
+import Foundation
+
+let gSettings = Settings()
+
+class Settings {
+    var sessionName : String = ""
+    var longitude : Double = 0.0
+    var latitude : Double = 0.0
+}
